d384db35dce3e3cb845332490aac16bb00853784
ValentinChCloud <chavaldranis@gmail.com>
2018-02-15 15:20:13 +0000

    Litle fix to allow write files

--- shiny-server.sh
+++ shiny-server.sh
@@ -3,7 +3,7 @@
 # Make sure the directory for individual app logs exists
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
-
+chown -R shiny.shiny /srv/shiny-server/sample-apps/SIG/wallace/shiny
 chmod +x /monitor_traffic.sh
 exec /monitor_traffic.sh &
 
